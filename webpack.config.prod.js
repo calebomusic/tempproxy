d8e28df3afd6dc30f56e500f70aa023e10fb16a2
calebomusic <calebomusic@gmail.com>
2017-03-04 13:36:23 -0800

    Update webpack.config.prod plugins

---
+++ webpack.config.prod.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require("webpack");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/tempproxy.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
